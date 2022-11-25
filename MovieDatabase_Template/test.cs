97d15280f2f65dcc454aa843e9bca72810cf7f55
Marcus Medina <medina.jobb@outlook.com>
2022-11-25 10:40:44 +0100

    Auto commit at 2022-11-25 10:40:44

---
+++ MovieDatabase_Template/test.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Minfil.cs" company="Mitt företag">
+// Copyright (c) 2012 All Rights Reserved,
+// Published under the MIT license.
+// </copyright>
+// <author>DU!</author>
+// <date>2022/12/24 15:05:00 AM </date>
+// <summary>Beskrivning av världens coolaste klass</summary>
+namespace MovieDatabase_Template
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name;
+        public int BornYear { get; set; }
+
+    }
+
+    public class test
+    {
+        public test()
+        {
+        }
+
+
+        
+
+    }
+}
